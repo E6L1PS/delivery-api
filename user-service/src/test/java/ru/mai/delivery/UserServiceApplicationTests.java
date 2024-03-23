ce21e65297f66f2e004323b583d2afbbb3b01f1c
Pesternikov Danil <danils003@mail.ru>
2024-03-23 13:56:51 +0300

    add: layered Dockerfile

--- user-service/src/test/java/ru/mai/delivery/UserServiceApplicationTests.java
+++ user-service/src/test/java/ru/mai/delivery/UserServiceApplicationTests.java
@@ -1,13 +1,8 @@
 package ru.mai.delivery;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class UserServiceApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
 
 }
