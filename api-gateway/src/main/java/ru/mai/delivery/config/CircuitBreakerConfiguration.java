f7de34a8ac5c925f85f4fd528c5b1dcecf9e0150
danil <danils003@mail.ru>
2024-06-05 14:37:51 +0300

    fix: cb to reactive

--- api-gateway/src/main/java/ru/mai/delivery/config/CircuitBreakerConfiguration.java
+++ api-gateway/src/main/java/ru/mai/delivery/config/CircuitBreakerConfiguration.java
@@ -2,6 +2,7 @@ package ru.mai.delivery.config;
 
 import io.github.resilience4j.circuitbreaker.CircuitBreakerConfig;
 import io.github.resilience4j.timelimiter.TimeLimiterConfig;
+import org.springframework.cloud.circuitbreaker.resilience4j.ReactiveResilience4JCircuitBreakerFactory;
 import org.springframework.cloud.circuitbreaker.resilience4j.Resilience4JCircuitBreakerFactory;
 import org.springframework.cloud.circuitbreaker.resilience4j.Resilience4JConfigBuilder;
 import org.springframework.cloud.client.circuitbreaker.Customizer;
@@ -19,7 +20,7 @@ import java.time.Duration;
 public class CircuitBreakerConfiguration {
 
     @Bean
-    public Customizer<Resilience4JCircuitBreakerFactory> defaultCustomizer() {
+    public Customizer<ReactiveResilience4JCircuitBreakerFactory> defaultCustomizer() {
         return factory -> factory.configureDefault(id -> new Resilience4JConfigBuilder(id)
                 .timeLimiterConfig(TimeLimiterConfig.custom()
                         .timeoutDuration(Duration.ofSeconds(1))
